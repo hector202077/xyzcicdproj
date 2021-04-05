79640ba6160b069d9cf3fdd1365aad2b383b6b4c
Hector R <hectormrg0823@gmail.com>
2021-04-05 05:44:37 -0700

    initial commit

---
+++ mymaven25/src/main/java/com/xyzweb/mymaven25/Mymaven25Application.java
@@ -0,0 +1,13 @@
+package com.xyzweb.mymaven25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mymaven25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mymaven25Application.class, args);
+	}
+
+}
---
+++ mymaven25/src/test/java/com/xyzweb/mymaven25/Mymaven25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzweb.mymaven25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mymaven25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
